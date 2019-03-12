27b62459e3041c30ea9cbc5fd0d40cd775880536
Chedak <cedomir.miskov@gmail.com>
2019-03-12 17:38:39 +0100

    Add callback method to user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,7 @@
 class User < ApplicationRecord
   validates :name, presence: true, length: { maximum: 50 }
 
+  before_save { self.email = email.downcase }
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
   validates :email, presence: true,
                     length: { maximum: 255 },
