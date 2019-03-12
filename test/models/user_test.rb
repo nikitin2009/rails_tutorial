cecd1cf34fc1d1318a9d6a5989476b07e59b08fa
Chedak <cedomir.miskov@gmail.com>
2019-03-12 17:40:58 +0100

    Add downcase test to user model

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -48,11 +48,18 @@ class UserTest < ActiveSupport::TestCase
     end
   end
 
-test "email addresses should be unique" do
-  duplicate_user = @user.dup
-  duplicate_user.email = @user.email.upcase
+  test "email addresses should be unique" do
+    duplicate_user = @user.dup
+    duplicate_user.email = @user.email.upcase
+    @user.save
+    assert_not duplicate_user.valid?
+  end
+
+test "email addresses should be saved as lower-case" do
+  mixed_case_email = "Foo@ExAMPle.CoM"
+  @user.email = mixed_case_email
   @user.save
-  assert_not duplicate_user.valid?
+  assert_equal mixed_case_email.downcase, @user.reload.email
 end
 
 end
