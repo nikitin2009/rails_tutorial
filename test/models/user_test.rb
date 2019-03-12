90dabf7acfe870f8fd5a43715925cdbec3ae81b3
Alex <nikitin2009@gmail.com>
2019-03-12 17:24:35 +0200

    Add validations and tests for User model

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -10,4 +10,24 @@ class UserTest < ActiveSupport::TestCase
     assert @user.valid?
   end
 
+  test "name should be present" do
+    @user.name = "     "
+    assert_not @user.valid?
+  end
+
+  test "email should be present" do
+    @user.email = "     "
+    assert_not @user.valid?
+  end
+
+  test "name should not be too long" do
+    @user.name = "a" * 51
+    assert_not @user.valid?
+  end
+
+  test "email should not be too long" do
+    @user.email = "a" * 244 + "@example.com"
+    assert_not @user.valid?
+  end
+
 end
