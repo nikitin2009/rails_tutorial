3e2423e6a2fe22f77fac8d730f12f8bdb980e299
Alex <nikitin2009@gmail.com>
2019-03-26 12:24:53 +0200

    Add Remember Me test

--- test/integration/users_login_test.rb
+++ test/integration/users_login_test.rb
@@ -38,5 +38,13 @@ class UsersLoginTest < ActionDispatch::IntegrationTest
     get root_path
     assert flash.empty?
   end
+
+  test "login with remembering" do
+    # Log in to set the cookie
+    log_in_as(@user, remember_me: '1')
+    # Log in again and verify that the cookie is deleted
+    log_in_as(@user, remember_me: '0')
+    assert_empty cookies[:remember_token]
+  end
   
 end
