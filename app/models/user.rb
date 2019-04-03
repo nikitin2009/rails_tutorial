77fa1f6fe217819aa90e463a36512abdbe83f758
Alex <nikitin2009@gmail.com>
2019-04-03 13:48:28 +0300

    Add Micropost model and tests for it

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,8 @@
 class User < ApplicationRecord
   attr_accessor :remember_token, :activation_token, :reset_token
 
+  has_many :microposts, dependent: :destroy
+
   before_save   :downcase_email
   before_create :create_activation_digest
 
